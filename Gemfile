0d4a8e0a608d202dbaedbb4b712686bc8c0bc418
Danny Guinther <danny@backupify.com>
2015-08-28 15:42:35 -0400

    Add test harness

--- Gemfile
+++ Gemfile
@@ -6,3 +6,12 @@ group :development, :test do
   gem "pry"
   gem "pry-byebug"
 end
+
+group :test do
+  gem "coveralls", :require => false
+  gem "guard"
+  gem "guard-minitest"
+  gem "minitest", ">= 3.0"
+  gem "mocha"
+  gem "simplecov", :require => false
+end
--- test/rbots/rbots_module_test.rb
+++ test/rbots/rbots_module_test.rb
@@ -1,9 +1,11 @@
-require "minitest_helper"
+require "test_helper"
 
 module Rbots::Test
-  class RbotsModuleTest < Minitest::Test
-    def test_that_it_has_a_version_number
-      refute_nil ::Rbots::VERSION
+  class RbotsTest < Rbots::TestCase
+    context "Rbots module" do
+      should "have a version" do
+        refute_nil ::Rbots::VERSION
+      end
     end
   end
 end
