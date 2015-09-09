15f09a32c36df7a150e6fea4f2a2823ba9ffc855
Danny Guinther <danny@backupify.com>
2015-09-09 09:25:42 -0400

    Add command to list no_deploy hosts

--- lib/rbots/plugin/no_deploy.rb
+++ lib/rbots/plugin/no_deploy.rb
@@ -7,9 +7,10 @@ module Rbots::Plugin
 
     DISABLE_DESC = "disable deploys to <host ip> - Disable deploys to workers on a host machine with a matching IP"
     ENABLE_DESC = "enable deploys to <host ip> - Enable deploys to workers on a host machine with a matching IP"
+    LIST_DESC = "list no deploy - List worker machines that are currently on the no deploy list"
 
     desc(DISABLE_DESC)
-    on(/(?:disable deploys to) (.*)/i) do |ip|
+    on(/(?:disable deploys? to) (.*)/i) do |ip|
       brain = Rbots::Brain.brain
       names = worker_names_for_ip(ip)
       names = [ip] if names.empty?
@@ -20,7 +21,7 @@ module Rbots::Plugin
     end
 
     desc(ENABLE_DESC)
-    on(/(?:enable deploys to) (.*)/i) do |ip|
+    on(/(?:enable deploys? to) (.*)/i) do |ip|
       names = worker_names_for_ip(ip)
       brain = Rbots::Brain.brain
       names = [ip] if names.empty?
@@ -28,6 +29,12 @@ module Rbots::Plugin
         brain.srem("no_deploy", worker_name)
       end
       reply("Enabled deploys to #{names.join(", ")}")
+    end
+
+    desc(LIST_DESC)
+    on(/(?:list no deploy)/i) do
+      names = Rbots::Brain.brain.smembers("no_deploy")
+      reply("The following hosts are on the no deploy list: #{names.join(", ")}")
     end
 
     def worker_names_for_ip(ip)
