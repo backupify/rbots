c01ee6f5854345d244449e99572b49262c698937
Danny Guinther <danny@backupify.com>
2015-09-09 16:28:21 -0400

    Try to handle lists of IPs or worker names

--- lib/rbots/plugin/no_deploy.rb
+++ lib/rbots/plugin/no_deploy.rb
@@ -1,4 +1,5 @@
 require "yaml"
+require "resolv"
 
 module Rbots::Plugin
   class NoDeploy
@@ -10,40 +11,66 @@ module Rbots::Plugin
     LIST_DESC = "list no deploy - List worker machines that are currently on the no deploy list"
 
     desc(DISABLE_DESC)
-    on(/(?:@[^ ]+ )?(?:disable deploys? to) (.*)/i) do |ip|
-      brain = Rbots::Brain.brain
-      names = worker_names_for_ip(ip)
-      names = [ip] if names.empty?
-      names.each do |worker_name|
-        brain.sadd("no_deploy", worker_name)
-      end
+    on(/(?:@[^ ]+ )?(?:disable deploys? to) (.*)/i) do |entities|
+      names = find_worker_names(entities)
+      toggle_hosts(names, :enabled => false)
       reply("Disabled deploys to #{names.join(", ")}")
     end
 
     desc(ENABLE_DESC)
-    on(/(?:@[^ ]+ )?(?:enable deploys? to) (.*)/i) do |ip|
-      names = worker_names_for_ip(ip)
-      brain = Rbots::Brain.brain
-      names = [ip] if names.empty?
-      names.each do |worker_name|
-        brain.srem("no_deploy", worker_name)
-      end
+    on(/(?:@[^ ]+ )?(?:enable deploys? to) (.*)/i) do |entities|
+      names = find_worker_names(entities)
+      toggle_hosts(names, :enabled => true)
       reply("Enabled deploys to #{names.join(", ")}")
     end
 
     desc(LIST_DESC)
     on(/(?:@[^ ]+ )?(?:list no deploy)/i) do
-      names = Rbots::Brain.brain.smembers("no_deploy")
-      reply("The following hosts are on the no deploy list: #{names.join(", ")}")
+      reply("The following hosts are on the no deploy list: #{disabled_hosts.join(", ")}")
     end
 
-    def worker_names_for_ip(ip)
-      names = self.class.worker_name_map[ip]
-      return names unless names.nil?
-      return [ip] if self.class.worker_name_map.values.flatten.include?(ip)
+    def brain
+      self.class.brain
     end
 
-    def self.worker_name_map
+    def disabled_hosts
+      Rbots::Brain.brain.smembers("no_deploy")
+    end
+
+    def extract_name(entity)
+      if is_ip?(entity)
+        worker_name_map.fetch(entity)
+      elsif known_hosts.include?(entity)
+        entity
+      else
+        nil
+      end
+    end
+
+    def find_worker_names(names_or_ips)
+      collection = names_or_ips.split(/,/)
+      collection.map! { |name_or_ip| extract_name(name_or_ip.strip) }
+      collection.compact!
+      collection
+    end
+
+    def is_ip?(str)
+      Resolv::IPv4::Regex === str
+    end
+
+    def known_hosts
+      @known_hosts ||= Set.new(worker_name_map.values.flatten)
+    end
+
+    def toggle_hosts(*names, options)
+      if options[:enabled]
+        brain.srem("no_deploy", *names)
+      else
+        brain.sadd("no_deploy", *names)
+      end
+    end
+
+    def worker_name_map
       @worker_name_map ||= YAML.load_file("/root/ip_map.yml")
     end
   end
